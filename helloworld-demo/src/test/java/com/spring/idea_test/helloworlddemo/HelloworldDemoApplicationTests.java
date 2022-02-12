30c9fad413058dc57268f375e853a33907cf0a54
chunjiemeng <chunjiemeng123@163.com>
2022-02-12 12:22:32 +0800

    Git commit from intellij idea

---
+++ helloworld-demo/src/test/java/com/spring/idea_test/helloworlddemo/HelloworldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.idea_test.helloworlddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
